ad63fc4f4bab72c0c334fe0d6467c5f896394557
W5Lang <dominikrober04@gmail.com>
2025-08-09 17:24:10 +0000

    try3_7

--- src/counter.js
+++ src/counter.js
@@ -1,9 +1,15 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `Gallery ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  element.innerHTML = `Random Cat Video`
+  element.style.cursor = 'pointer'
+  const catVideos = [
+    "https://www.youtube.com/watch?v=J---aiyznGQ",
+    "https://www.youtube.com/watch?v=hY7m5jjJ9mM",
+    "https://www.youtube.com/watch?v=5dsGWM5XGdg",
+    "https://www.youtube.com/watch?v=FtutLA63Cp8",
+    "https://www.youtube.com/watch?v=CbUhz8m1v7w"
+  ]
+  element.addEventListener('click', () => {
+    const randomUrl = catVideos[Math.floor(Math.random() * catVideos.length)]
+    window.open(randomUrl, '_blank')
+  })
 }
