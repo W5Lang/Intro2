beac438ece17b29e63be1d2cd99afda672ed84ef
W5Lang <dominikrober04@gmail.com>
2025-08-09 17:30:00 +0000

    try3_8

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import javascriptLogo from './5paedy.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/5paedy.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
